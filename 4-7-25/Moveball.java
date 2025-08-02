412a7a9e8eb2c750788a15f5b41a18ed1c3b752e
Ganeshghode <152708594+Ganeshghode@users.noreply.github.com>
2025-08-02 11:52:09 +0530

    Add files via upload

---
+++ 4-7-25/Moveball.java
@@ -0,0 +1,30 @@
+class Moveball 
+{
+	public static void main(String args[])
+{
+	int a=1;
+	String step="";
+	a++;
+	step+=a + "\n";
+	a++;
+	step+=a + "\n";
+	a++;
+	step+=a + "\n";
+	a++;
+	step+=a + "\n";
+	a++;
+	step+=a+"\n";
+	a--;
+	step+=a + "\n";
+	a--;
+	step+=a + "\n";
+	a--;
+	step+=a + "\n";
+	a--;
+	step+=a + "\n";
+	a--;
+	step+=a+"\n";
+	System.out.println(step);
+}
+	}
+
