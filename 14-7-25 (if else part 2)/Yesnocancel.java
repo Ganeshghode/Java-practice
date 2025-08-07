21417fa7c4abc3333db5583038776285b478aea8
Ganeshghode <152708594+Ganeshghode@users.noreply.github.com>
2025-08-07 23:54:13 +0530

    Add files via upload

---
+++ 14-7-25 (if else part 2)/Yesnocancel.java
@@ -0,0 +1,16 @@
+class Yesnocancel
+{
+	public static void main(String args[])
+	{
+		char ch = 'c';
+		if(ch =='y')
+		System.out.println("Yes");
+		else if(ch =='n')
+		System.out.println("No");
+		else if(ch =='c')
+		System.out.println("Cancel");
+		else
+		System.out.print("invalid);
+	}
+
+}
