ec567a5c311b9bb38e66d296bb8b6337f691fd7b
Ganeshghode <152708594+Ganeshghode@users.noreply.github.com>
2025-08-07 23:52:44 +0530

    Add files via upload

---
+++ 7-7-25 (operator part1)/Parse.java
@@ -0,0 +1,13 @@
+class Parse {
+public static void main(String args[])
+{
+	/*numeric string to int,float,double*/
+	String s="12345";
+	int a=Integer.parseInt(s);
+	float b=Float.parseFloat(s);
+	double c=Double.parseDouble(s);
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+}}
+	
